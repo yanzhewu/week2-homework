cc8609da9a2750ba10b4ae5c32481d9775f9e615
wuyanzhe <yanzhewu@uchicago.edu>
2015-04-12 21:20:41 -0500

    no message

--- hw/config/routes.rb
+++ hw/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/greet' => 'greet#show'
   get '/contact' => 'contact#enter'
-  get '/contact/contact_submitted' => 'contact#submit'
+  get '/contact_submitted' => 'contact#submit'
   get '/dice' => 'dice#play'
   get '/dice/roll' => 'dice#roll'
   get '/rps' => 'rps#game'
